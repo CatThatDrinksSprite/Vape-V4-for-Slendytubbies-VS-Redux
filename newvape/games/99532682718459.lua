73866caa9766a5056f196f0f53f31ad5829e0b73
CatThatDrinksSprite <100092400+CatThatDrinksSprite@users.noreply.github.com>
2026-01-12 08:51:04 -0500

    Implement AttackRange feature in Killaura module
    Added AttackRange slider to Killaura module and updated attack logic to use it.

--- newvape/games/99532682718459.lua
+++ newvape/games/99532682718459.lua
@@ -1,16 +1,17 @@
 local vape = shared.vape
 local Killaura
+local AttackRange
 
 Killaura = vape.Categories.Combat:CreateModule({
     Name = "Killaura",
     Function = function(callback)
-        repeat task.wait()
-            task.spawn(function()
-                for _, Player in ipairs(workspace:WaitForChild("game"):WaitForChild("Players"):GetChildren()) do
-                    if Player:GetAttribute("isChar") and game:GetService("Players").LocalPlayer.Character:GetAttribute("monsterType") and (Player:WaitForChild("HumanoidRootPart").Position - game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).Magnitude <= 12 then
+        repeat wait(0.5)
+            spawn(function()
+                for _, Player in ipairs(game:GetService("Players"):GetPlayers()) do
+                    if Player.Character and Player.Character:GetAttribute("Player") and game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:GetAttribute("monsterType") and (Player.Character:WaitForChild("HumanoidRootPart").Position - game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).Magnitude <= AttackRange.Value then
                         game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("sendAttack"):FireServer(unpack({
                             {
-                                Player
+                                Player.Character
                             },
                             game:GetService("Players").LocalPlayer.Character:GetAttribute("monsterType"),
                         }))
@@ -19,5 +20,15 @@ Killaura = vape.Categories.Combat:CreateModule({
             end)
         until (not Killaura.Enabled)
     end,
-    Tooltip = "Hits Every Guardian Near You.",
+    Tooltip = "Attacks every Guardian near You",
+})
+
+AttackRange = Killaura:CreateSlider({
+    Name = "Attack Range",
+    Min = 1,
+    Max = 30,
+    Default = 13,
+    Suffix = function(val)
+        return val == 1 and 'stud' or 'studs'
+    end,
 })
